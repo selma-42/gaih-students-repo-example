b2193a0f219f38a07578d43336228fd19986ca44
selma-42 <74605840+selma-42@users.noreply.github.com>
2021-03-14 23:36:03 +0300

    Add files via upload

---
+++ HOMEWORK2.py
@@ -0,0 +1,39 @@
+CV = {"cv_1": {"isim": "AYŞE",
+                     "memleket": "ADIYAMAN",
+                     "Yaş": "21",
+                     "Öğrenim Durumu": "Lisans",
+                     "Başvurduğu Statü": "MEKATRONİK MÜHENDİSLİĞİ"},
+
+
+           "cv_2": {"isim": "ELİF" ,
+                               "memleket": "SİVAS" ,
+                               "Yaş"     : "25" ,
+                               "Öğrenim Durumu": "Lisans" ,
+                               "Başvurduğu Statü": "ELEKTRİK MÜHENDİSLİĞİ"},
+
+
+           "cv_3": {"isim": "ALİ" ,
+                               "memleket": "ORDU" ,
+                               "Yaş"     : "32" ,
+                               "Öğrenim Durumu": "Lisans" ,
+                               "Başvurduğu Statü": "İNSAN KAYNAKLARI "},
+
+           "cv_4": {"isim": "MUSTAFA" ,
+                               "memleket": "İZMİR" ,
+                               "Yaş"     : "33" ,
+                               "Öğrenim Durumu": "Lisans" ,
+                               "Başvurduğu Statü": "HARİTA MÜHENDİSLİĞİ"},
+
+           "cv_5": {"isim": "ELİF" ,
+                               "memleket": "BURSA" ,
+                               "Yaş"     : "26" ,
+                               "Öğrenim Durumu": "Lisans" ,
+                               "Başvurduğu Statü": "YAZILIM MÜHENDİSLİĞİ"}
+           }
+
+
+print(CV["cv_1"])
+print(CV["cv_2"])
+print(CV["cv_3"])
+print(CV["cv_4"])
+print(CV["cv_5"])
